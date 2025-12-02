246e70b60f6fc34da5a9be3edb0495473c451715
Henrique Santos Silva <henriqueesquecemuito@gmail.com>
2025-12-01 21:21:44 -0300

    Initial

--- index.c
+++ index.c
@@ -14,5 +14,32 @@ typedef struct
 // FUNÇÃO PRINCIPAL
 int main(void)
 {
+
+    Contato c[3] = {
+        {"Ana", 19},
+        {"Bruno", 23},
+        {"Clara", 25},
+    };
+
+    // Adicionar Dados
+    //  for (int i = 0; i < 3; i++)
+    //  {
+    //      printf("Nome: ");
+    //      scanf("%s", c[i].nome);
+
+    //     printf("Idade: ");
+    //     scanf("%i", &c[i].idade);
+    // }
+
+    // Printar Dados
+    //  printf("--Dados--\n");
+
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     printf("Nome: %s\n", c[i].nome);
+    //     printf("Idade: %i\n", c[i].idade);
+
+    // }
+
     return 0;
 }
