7beabfeff585683e71c18abbdd9c7490a8dd38a3
Henrique Silva <143834219+Henrique-nte@users.noreply.github.com>
2025-12-02 15:33:55 -0300

    Alter

---
+++ contato.h
@@ -0,0 +1,10 @@
+#ifndef CONTATO_H
+#define CONTATO_H
+
+typedef struct
+{
+  char nome[50];
+  int idade;
+} Contato;
+
+#endif
--- index.c
+++ index.c
@@ -4,22 +4,71 @@
 // BIBLIOTECAS UTILIZADAS
 #include <stdio.h>
 #include <stdlib.h>
+#include "contato.h" //Incluo o struct Contato que criei em outro arquivo
 
-typedef struct
+// FUNCÃO ADICIONAR CONTATO
+//  add_contato(char nome, int idade){
+//      printf("Hello World!!\n");
+//  };
+
+// Função que mostra o menu e retorna a opção escolhida pelo usuário
+int show_Menu()
 {
-    char nome[50];
-    int idade;
-} Contato;
+    int opc;
+    printf("----MENU----\n");
+    printf("----ACOES----\n");
+    printf("1 - CONSULTAR CONTATO\n");
+    printf("2 - INCLUIR CONTATO\n");
+    printf("3 - EXCLUIR CONTATO\n");
+    printf("4 - SAIR\n");
+    scanf("%i", &opc);
+    return opc;
+}
+
+void show_Contats(Contato c)
+{
+    printf("Mostrar Contatos!\n");
+}
 
 // FUNÇÃO PRINCIPAL
 int main(void)
 {
-
     Contato c[3] = {
         {"Ana", 19},
         {"Bruno", 23},
         {"Clara", 25},
     };
+    int opc;
+    // O programa deve apresentar um menu com, no mínimo, as seguintes opções:
+    // Incluir contato
+    // Consultar contato
+    // Excluir contato
+    // Sair
+    do
+    {
+        opc = show_Menu();
+
+        if (opc != 1 && opc != 2 && opc != 3 && opc != 4)
+        {
+            system("cls");
+            printf("Opcao invalida!!\n");
+        }
+
+    } while (opc != 1 && opc != 2 && opc != 3 && opc != 4);
+
+    if (opc == 4)
+        printf("Saiu!\n");
+
+    // Case para opções
+    switch (opc)
+    {
+    case 1:
+        show_Contats(*c);
+        break;
+
+    default:
+        break;
+    }
 
     // Adicionar Dados
     //  for (int i = 0; i < 3; i++)
@@ -36,6 +85,7 @@ int main(void)
 
     // for (int i = 0; i < 3; i++)
     // {
+    //     printf("---CONTATO %i---\n", i+1);
     //     printf("Nome: %s\n", c[i].nome);
     //     printf("Idade: %i\n", c[i].idade);
 
